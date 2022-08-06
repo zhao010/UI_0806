7241b674fe26e258a4d18f9a9377b59db925e53e
zhao010 <17600570787@163.com>
2022-08-07 03:35:21 +0800

    新添加的文件内容描述

--- Mydriver/MYdriver.py
+++ Mydriver/MYdriver.py
@@ -21,7 +21,8 @@ def my_driver():
     opt.add_argument('--disable-gpu')
     opt.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
     #opt.add_argument("--remote-debugging-port=9222")
-    mdriver = webdriver.Chrome(executable_path = '/usr/bin/chromedriver',chrome_options=opt)
+    mdriver = webdriver.Chrome(chrome_options=opt)
+    #executable_path = '/usr/bin/chromedriver'
     mdriver.maximize_window()
     mdriver.implicitly_wait(10)
     return mdriver
