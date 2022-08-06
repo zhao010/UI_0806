7c656d86e85391289d2806a1c5a6eab46a64e4c4
zhao010 <17600570787@163.com>
2022-08-06 23:30:13 +0800

    新添加的文件内容描述

--- Mydriver/MYdriver.py
+++ Mydriver/MYdriver.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from selenium import webdriver
+
 import os
 
 
@@ -13,15 +13,15 @@ def my_driver():
     opt = Options()
     # 把chrome设置成无界面模式，不论windows还是linux都可以，自动适配对应参数
     #opt.set_headless()
-    CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
-    GOOGLE_CHROME_SHIM = os.getenv('GOOGLE_CHROME_SHIM', "chromedriver")
+    #CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
+    #GOOGLE_CHROME_SHIM = os.getenv('GOOGLE_CHROME_SHIM', "chromedriver")
     opt.add_argument('--headless')
     opt.add_argument('--no-sandbox')
     opt.add_argument('--disable-dev-shm-usage')
     opt.add_argument('--disable-gpu')
     opt.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
     #opt.add_argument("--remote-debugging-port=9222")
-    mdriver = webdriver.Chrome(executable_path = GOOGLE_CHROME_SHIM,chrome_options=opt)
-    #mdriver.maximize_window()
+    mdriver = webdriver.Chrome(executable_path = '/usr/bin/chromedriver',chrome_options=opt)
+    mdriver.maximize_window()
     mdriver.implicitly_wait(10)
     return mdriver
