3eefd8f8b539c9f9c7389a9007acb59d36f98bfc
zhao010 <17600570787@163.com>
2022-08-06 22:36:32 +0800

    新添加的文件内容描述

--- Mydriver/MYdriver.py
+++ Mydriver/MYdriver.py
@@ -5,15 +5,17 @@ from selenium.webdriver.chrome.options import Options
 
 def my_driver():
     # 创建chrome参数对象
-    opt = webdriver.ChromeOptions()
+    #opt = webdriver.ChromeOptions()
+    opt = Options()
     # 把chrome设置成无界面模式，不论windows还是linux都可以，自动适配对应参数
     #opt.set_headless()
     opt.add_argument('--headless')
     opt.add_argument('--no-sandbox')
+    opt.add_argument('--disable-dev-shm-usage')
     opt.add_argument('--disable-gpu')
     opt.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
-    opt.add_argument("--remote-debugging-port=9222")
-    mdriver = webdriver.Chrome('/usr/bin/chromedriver',options=opt)
+    #opt.add_argument("--remote-debugging-port=9222")
+    mdriver = webdriver.Chrome(executable_path = '/usr/bin/chromedriver',chrome_options=opt)
     mdriver.maximize_window()
     mdriver.implicitly_wait(10)
     return mdriver
